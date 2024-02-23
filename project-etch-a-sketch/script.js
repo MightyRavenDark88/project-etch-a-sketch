cbe5707d3294f9a4610591bc0ae02a99f3260177
Eric <kinggreenbong@gmail.com>
2024-02-23 12:16:42 -0700

    Create first draft of javascript algorithm

---
+++ project-etch-a-sketch/script.js
@@ -0,0 +1,40 @@
+//declare body container reference
+//declare variable for mouseout event as false
+
+//create box div element
+//apply .box class with classlist --> styles referred to in css
+
+//create header div for title of the game
+//append header div to body
+//apply .header class with classlist --> styles referred to in css
+
+//create button to set number of squares
+//append play button to header div
+//apply .button class with classlist --> styles referred to in css
+
+//append box div to body
+
+//add mouseover event listener to make squares change color
+    //declare red, green, and blue variables using Math.random to randomize color value for background color rgb input
+
+    //if statement to check for whether square color changed
+        //squares set to random color based off red, green, blue variables
+    //else color darkening per interaction
+
+//add mouseout event listener to set status of color change
+    //set mouseout variable to true
+
+//add event listener to create grid and start game of etch-a-sketch
+    
+    //browser will prompt user for row*column dimension number
+    
+    //create user specified number of row div elements *loop
+            
+            //create user specified number of square div elements *loop
+                //createElement call for squares
+                //append square divs in row div
+                //apply .square class with classlist --> styles referred to in css
+        
+        //createElement call for rows
+        //append all row divs in box div
+        //apply .row class with classlist --> styles referred to in css
