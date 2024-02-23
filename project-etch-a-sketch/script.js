509a3aef716bb5466b2035bf2256b132955febf9
Eric <kinggreenbong@gmail.com>
2024-02-23 12:49:04 -0700

    Create and append page structure elements
    Creates elements for general page structure layout such as header, play
    button, and game box; appends them to the body; and defines a variable
    for future mouseover event listener.

--- project-etch-a-sketch/script.js
+++ project-etch-a-sketch/script.js
@@ -1,18 +1,34 @@
 //declare body container reference
+const body = document.querySelector("body");
+
 //declare variable for mouseout event as false
+let isColorChanged = false;
 
 //create box div element
+const box = document.createElement("div");
 //apply .box class with classlist --> styles referred to in css
+box.classList("box");
 
 //create header div for title of the game
+const header = document.createElement("div");
+//add header text content
+header.textContent = "Etch-A-Sketch";
 //append header div to body
+body.append(header);
 //apply .header class with classlist --> styles referred to in css
+header.classList("header");
 
 //create button to set number of squares
+const dimButton = document.createElement("button");
+//add button text content
+dimButton.textContent = "Play";
 //append play button to header div
+header.append(dimButton);
 //apply .button class with classlist --> styles referred to in css
+dimButton.classList("button");
 
 //append box div to body
+body.append(box);
 
 //add mouseover event listener to make squares change color
     //declare red, green, and blue variables using Math.random to randomize color value for background color rgb input
